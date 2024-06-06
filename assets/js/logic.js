b6bdd633a30e2702f9a6c381c65b9d2363699657
jmeason <121059050+jmeason@users.noreply.github.com>
2024-06-05 23:03:08 -0400

    Update logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,29 @@
+// this goes with the blog page (index.html) and the styles.css for light/dark
+
+//Dependencies
+//from blog.js
+// const postsList=document.getElementById('posts-list')
+// const toggleMode=document.getElementById('toggle-mode')
+// const backButton=document.getElementById('back-button')
+
+    
+    //Data
+    
+    
+    //Funciton
+    document.addEventListener('DOMContentLoaded', () => {
+        const isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+        if (isDarkMode) {
+          document.body.classList.add('dark-mode');
+          document.body.classList.remove('light-mode');
+        } else {
+          document.body.classList.add('light-mode');
+          document.body.classList.remove('dark-mode');
+        }
+      
+        // Save the theme preference
+        document.getElementById('toggle-mode')?.addEventListener('click', () => {
+          const isDarkMode = document.body.classList.contains('dark-mode');
+          localStorage.setItem('isDarkMode', isDarkMode);
+        });
+      });
